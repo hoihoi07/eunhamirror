bfe7b96e7347b45c794a6a8c399242bd6628199a
hoihoi07 <97010331+hoihoi07@users.noreply.github.com>
2022-05-30 11:00:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -59,8 +59,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Channel", "https://t.me/Namexian")
-    buttons.buildbutton("Group", "https://t.me/EunhaMirror")
+    buttons.buildbutton("Channel", "https://t.me/jetbots")
+    buttons.buildbutton("Owner", "https://t.me/jetbots_bot")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
